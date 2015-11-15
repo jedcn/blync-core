779e192c21139d6fce5a538c3c93088cb7abbd6e
Jed Northridge <git@jedcn.com>
2015-11-15 08:49:58 -0500

    And now getting rid of index-spec.js

--- spec/index-spec.js
+++
@@ -1,9 +0,0 @@
-const newBlyncDeviceManager = require('../index');
-
-xdescribe('requiring index.js', function() {
-  it('returns a new BlyncDeviceManager', function() {
-    expect(newBlyncDeviceManager).toBeDefined();
-    expect(newBlyncDeviceManager.findFirstBlyncLight)
-      .toEqual(jasmine.any(Function));
-  })
-});
