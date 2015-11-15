0171911cf066dff73b10d0c6daae0b10f44c9a0b
Jed Northridge <git@jedcn.com>
2015-11-15 10:48:29 -0500

    Revert "Delete blync-light-manager-spec"
    This reverts commit a8b4a2ed7f9173ca59add833d6f6a3f685c624d5.
    
    I removed this file initially because I was trying to see if I could
    build on CircleCi and Travis without it.
    
    It's problematic because it involves 'node-hid', and code associated
    with node-hid doesn't run on the ubuntu containers on these CI systems.
    
    I have a plan for running these specs now, and so I'm bringing it back.

---
+++ spec/lib/blync-light-manager-spec.js
@@ -0,0 +1,27 @@
+const BlyncLightManager = require('../../lib/blync-light-manager');
+const HID = require('node-hid');
+
+const hidPath = 'USB_0e53_2516_14100000';
+
+describe('BlyncLightManager', function() {
+  describe('#findFirstBlyncLight', function() {
+    it('works with node-hid to find a light', function() {
+      const manager = new BlyncLightManager()
+      spyOn(HID, 'devices').and.returnValue([{
+        vendorId: 3667,
+        productId: 9494,
+        path: hidPath,
+        serialNumber: '',
+        manufacturer: '',
+        product: 'Blynclight',
+        release: 256,
+        interface: -1 }])
+      blyncLightSpy = jasmine.createSpy();
+      spyOn(HID, 'HID').and.returnValue(blyncLightSpy);
+      const blyncLight = manager.findFirstBlyncLight();
+      expect(HID.devices).toHaveBeenCalled();
+      expect(HID.HID).toHaveBeenCalledWith(hidPath)
+      expect(blyncLight.device).toBe(blyncLightSpy);
+    });
+  });
+});
