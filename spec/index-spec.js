d6dd4b687512e3783a99c4826b67de40f9303784
Jed Northridge <git@jedcn.com>
2015-11-15 08:44:01 -0500

    Pend index-spec
    I don't actually need node-hid at test time. There are two specs that
    use it.

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,6 +1,6 @@
 const newBlyncDeviceManager = require('../index');
 
-describe('requiring index.js', function() {
+xdescribe('requiring index.js', function() {
   it('returns a new BlyncDeviceManager', function() {
     expect(newBlyncDeviceManager).toBeDefined();
     expect(newBlyncDeviceManager.findFirstBlyncLight)
