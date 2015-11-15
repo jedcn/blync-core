ec1dcdd902874de1b65522c5847d691dc0b664cd
Jed Northridge <git@jedcn.com>
2015-11-15 10:52:38 -0500

    Make blink-light-manager specs run conditionally

--- spec/lib/blync-light-manager-spec.js
+++ spec/lib/blync-light-manager-spec.js
@@ -1,27 +1,36 @@
-const BlyncLightManager = require('../../lib/blync-light-manager');
-const HID = require('node-hid');
+'use strict';
 
-const hidPath = 'USB_0e53_2516_14100000';
+function runTheseTests() {
+  const BlyncLightManager = require('../../lib/blync-light-manager');
+  const HID = require('node-hid');
 
-describe('BlyncLightManager', function() {
-  describe('#findFirstBlyncLight', function() {
-    it('works with node-hid to find a light', function() {
-      const manager = new BlyncLightManager()
-      spyOn(HID, 'devices').and.returnValue([{
-        vendorId: 3667,
-        productId: 9494,
-        path: hidPath,
-        serialNumber: '',
-        manufacturer: '',
-        product: 'Blynclight',
-        release: 256,
-        interface: -1 }])
-      blyncLightSpy = jasmine.createSpy();
-      spyOn(HID, 'HID').and.returnValue(blyncLightSpy);
-      const blyncLight = manager.findFirstBlyncLight();
-      expect(HID.devices).toHaveBeenCalled();
-      expect(HID.HID).toHaveBeenCalledWith(hidPath)
-      expect(blyncLight.device).toBe(blyncLightSpy);
+  const hidPath = 'USB_0e53_2516_14100000';
+
+  describe('BlyncLightManager', function() {
+    describe('#findFirstBlyncLight', function() {
+      it('works with node-hid to find a light', function() {
+        const manager = new BlyncLightManager()
+        spyOn(HID, 'devices').and.returnValue([{
+          vendorId: 3667,
+          productId: 9494,
+          path: hidPath,
+          serialNumber: '',
+          manufacturer: '',
+          product: 'Blynclight',
+          release: 256,
+          interface: -1 }])
+        blyncLightSpy = jasmine.createSpy();
+        spyOn(HID, 'HID').and.returnValue(blyncLightSpy);
+        const blyncLight = manager.findFirstBlyncLight();
+        expect(HID.devices).toHaveBeenCalled();
+        expect(HID.HID).toHaveBeenCalledWith(hidPath)
+        expect(blyncLight.device).toBe(blyncLightSpy);
+      });
     });
   });
-});
+
+}
+
+if (SHOULD_RUN_HID_TESTS) {
+  runTheseTests();
+}
